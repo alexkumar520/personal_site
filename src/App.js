02620c3e1a0736b0894575ae15c77b4f6fcd41c7
alexkumar520 <43835304+alexkumar520@users.noreply.github.com>
2020-06-21 00:43:50 -0700

    testing scroll functionality

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React from 'react';
 import SpotifyImage from './SpotifyImage'
 import './welcome.css';
 import alex from './alex.jpg';
+import {Link} from 'react-scroll';
 
 
 function App() {
   return (
     <>
+    <nav class="navbar navbar-expand-lg navbar-custom ">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a class="nav-link">Left Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link">Left Link 2</a>
+    </li>
+  </ul>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="#box">About Me</a>
+    </li>
+      <a class="nav-link" href="#">Contact Me</a>
+    
+  </ul>
+</nav>
     <div className="welcome">
       <a className="banner-text"> Hi, I'm Alex Kumar! </a>
       <br></br>
@@ -16,9 +35,33 @@ function App() {
       <a className="new-text"> Insert links/images here</a>
       <br></br>
 
-      <a className ="listening"> Here's what I'm listening to right now...</a>
+      
+    
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br><br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <a className ="listening"> Here's what I'm listening to right now...</a>
     </div>
     <SpotifyImage />
+    
     
     </>
   )
